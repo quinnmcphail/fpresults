dda03245cd5524f6e3c31d5f437680467491ce56
Quinn McPhail <quinnmcphail@me.com>
2018-10-27 07:57:53 -0400

    Changed to Brew Fishers

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends Component {
       results: [[]],
       current_results_page: 0,
       fields: [[]],
-      eventID: 110758,
+      eventID: 111230,
       eventName: null
     }
   }
