ad92200dfd3416227d9cffa90a6e33dfb603c77a
Quinn McPhail <quinnmcphail@me.com>
2018-11-10 21:07:20 -0500

    Changed to Honor The Brave

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends Component {
       results: [[]],
       current_results_page: 0,
       fields: [[]],
-      eventID: 111230,
+      eventID: 112225,
       eventName: null
     }
   }
@@ -188,8 +188,8 @@ class App extends Component {
               </div>
             </div>
             <div id="info-column" className="column has-text-centered is-one-quarter">
-              <img src='https://131events.com/wp-content/uploads/2016/02/brew-halloween-logo-horizontal.png' style={{marginTop: '20px', width: '300px'}} alt=''key="eventImage"/>
-              <h1 className="title is-3" style={{marginTop: '20px'}} key="eventName">Brew HaHa Halloween 5K</h1>
+              <img src='https://2tr8so2439vc34xre1h2sdi1-wpengine.netdna-ssl.com/wp-content/uploads/2016/11/honor-the-brave-logo.png' style={{marginTop: '20px', width: '300px'}} alt=''key="eventImage"/>
+              <h1 className="title is-3" style={{marginTop: '20px'}} key="eventName">Honor The Brave 5K</h1>
               {/* <h1 className="title is-3" key="raceName"></h1> */}
               <h2 className="subtitle is-5" key="eventResults">Results</h2>
               {/* <div id="spotify">
