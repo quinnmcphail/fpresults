d7a4de307f95cc2a096659e1261ab1dd47e38316
Quinn McPhail <quinnmcphail@me.com>
2019-03-23 08:41:18 -0400

    Spring Clean

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 // import url from 'url';
 import './App.css';
@@ -22,19 +22,19 @@ class App extends Component {
       results: [[]],
       current_results_page: 0,
       fields: [[]],
-      eventID: 113002,
+      eventID: 116096,
       eventName: null
     }
   }
   refreshToken = () => {
     axios
       .get('https://radiant-forest-25626.herokuapp.com/refreshTokens', {
-      params: {
-        refresh_token: this.state.refresh_token
-      }
-    })
+        params: {
+          refresh_token: this.state.refresh_token
+        }
+      })
       .then(e => {
-        this.setState({access_token: e.data.access_token, expires_in: e.data.expires_in})
+        this.setState({ access_token: e.data.access_token, expires_in: e.data.expires_in })
         setTimeout(this.refreshToken, this.state.expires_in * 1000)
       })
       .catch(err => {
@@ -84,11 +84,11 @@ class App extends Component {
     let that = this
     Repeat(function (done) {
       axios
-        .get('https://radiant-forest-25626.herokuapp.com/results/'+that.state.eventID)
+        .get('https://radiant-forest-25626.herokuapp.com/results/' + that.state.eventID)
         .then(e => {
           let fields = []
           let results = []
-          let resultsArray=[]
+          let resultsArray = []
           e
             .data
             .list
@@ -110,90 +110,90 @@ class App extends Component {
               }
               return temp
             })
-          while(resultsArray.length>0){
-            results.push(resultsArray.splice(0,14))
+          while (resultsArray.length > 0) {
+            results.push(resultsArray.splice(0, 14))
           }
-          that.setState({fields, results})
+          that.setState({ fields, results })
           done()
         })
         .catch(err => {
           console.error(err)
         })
-      })
+    })
       .every(5, 'sec')
       .start
       .now()
-      // Repeat(function(done){
-      //   axios.get('https://radiant-forest-25626.herokuapp.com/eventName/'+that.state.eventID).then(e=>{
-      //     console.log(e)
-      //     that.setState({eventName: e.data})
-      //     done()
-      //   }).catch(err=>{
-      //     console.error(err)
-      //   })
-      // })
-      // .every(30, 'sec')
-      // .start
-      // .now()
-      Repeat(function () {
-        if(that.state.results.length - 1 > that.state.current_results_page){
-          that.setState({current_results_page: that.state.current_results_page + 1})
-        }else{
-          that.setState({current_results_page: 0})
-        }
-      })
+    // Repeat(function(done){
+    //   axios.get('https://radiant-forest-25626.herokuapp.com/eventName/'+that.state.eventID).then(e=>{
+    //     console.log(e)
+    //     that.setState({eventName: e.data})
+    //     done()
+    //   }).catch(err=>{
+    //     console.error(err)
+    //   })
+    // })
+    // .every(30, 'sec')
+    // .start
+    // .now()
+    Repeat(function () {
+      if (that.state.results.length - 1 > that.state.current_results_page) {
+        that.setState({ current_results_page: that.state.current_results_page + 1 })
+      } else {
+        that.setState({ current_results_page: 0 })
+      }
+    })
       .every(12, 'sec')
       .start
       .in(12, 'sec')
   }
   render() {
     return this.state.spotify_auth
-      ? <div className="container is-fluid" style={{margin:0}}>
-          <div className="columns">
-            <div className="column is-three-quarters">
-              <div className="content is-large">
-                <ReactCSSTransitionReplace transitionName="cross-fade" transitionEnterTimeout={1000} transitionLeaveTimeout={1000}>
-                  <table className="table" key={`results-${this.state.current_results_page}`}>
-                    <thead id="resultsHead">
-                      <tr>
-                        {this
-                          .state
-                          .fields
-                          .map((e,i) => {
-                            return <th key={`header-${i}`}>{e}</th>
-                          })}
-                      </tr>
-                    </thead>
-                      <tbody id="results">
-                      {this.state.results[this.state.current_results_page] ?
+      ? <div className="container is-fluid" style={{ margin: 0 }}>
+        <div className="columns">
+          <div className="column is-three-quarters">
+            <div className="content is-large">
+              <ReactCSSTransitionReplace transitionName="cross-fade" transitionEnterTimeout={1000} transitionLeaveTimeout={1000}>
+                <table className="table" key={`results-${this.state.current_results_page}`}>
+                  <thead id="resultsHead">
+                    <tr>
+                      {this
+                        .state
+                        .fields
+                        .map((e, i) => {
+                          return <th key={`header-${i}`}>{e}</th>
+                        })}
+                    </tr>
+                  </thead>
+                  <tbody id="results">
+                    {this.state.results[this.state.current_results_page] ?
                       this
                         .state
                         .results[this.state.current_results_page]
-                        .map((e,index) => {
-                          return <tr key={`row-${index}`}>{e.map((i,index) => {
-                              return <td key={`cell-${index}`} className={this.state.fields[index]==='Age Group Place'?`agPlace${i}`:null}>
-                                <span>{i}</span>
-                              </td>
-                            })}</tr>
-                        }):<tr><td>Results Not Available</td></tr>}
-                    </tbody>
-                  </table>
-                  </ReactCSSTransitionReplace>
-              </div>
+                        .map((e, index) => {
+                          return <tr key={`row-${index}`}>{e.map((i, index) => {
+                            return <td key={`cell-${index}`} className={this.state.fields[index] === 'Age Group Place' ? `agPlace${i}` : null}>
+                              <span>{i}</span>
+                            </td>
+                          })}</tr>
+                        }) : <tr><td>Results Not Available</td></tr>}
+                  </tbody>
+                </table>
+              </ReactCSSTransitionReplace>
             </div>
-            <div id="info-column" className="column has-text-centered is-one-quarter">
-              <img src='http://nczal23rn1d20x8xm3fw5gb1-wpengine.netdna-ssl.com/wp-content/uploads/2016/04/bfth-logo.png' style={{marginTop: '20px', width: '300px'}} alt=''/>
-              <h1 className="title is-3">Bolt for the Heart 5K</h1>
-              <h2 className="subtitle is-5">Results</h2>
-              {/* <div id="spotify">
+          </div>
+          <div id="info-column" className="column has-text-centered is-one-quarter">
+            <img src='https://static1.squarespace.com/static/597d36d9197aea062b6ec7ad/598740b18419c22ddf2c2bd1/5c544892085229926936a9af/1551477693170/SCR+300dpi.jpg?format=1500w' style={{ marginTop: '20px', width: '300px' }} alt='' />
+            <h1 className="title is-3">Spring Clean 5K</h1>
+            <h2 className="subtitle is-5">Results</h2>
+            {/* <div id="spotify">
                 <h2 className="title is-5" style={{color:'#1ED760'}}><i className="fa fa-spotify" aria-hidden="true"></i> Now Playing</h2>
                 <img id="spotify-img" src={this.state.current_track_img} alt=''/>
                 <h3 className="title is-6 spotify-info" style={{color:'white'}}>{this.state.current_track_name}</h3>
                 <h3 className="title is-6 spotify-info" style={{color:'white'}}>{this.state.current_track_artist}</h3>
               </div> */}
-            </div>
           </div>
         </div>
+      </div>
       : <div>
         Loading
       </div>
