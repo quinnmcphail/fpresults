80a93cd906362833ee7c830244f04dbe36248ecc
Quinn McPhail <quinnmcphail@me.com>
2019-06-29 20:15:54 -0400

    Summer Night

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
-import * as donutLogo from "./donut.png";
+import * as summerLogo from "./summerNight.png";
 import * as Logo from "./resultsBack.svg";
 import Repeat from "repeat";
 import ReactCSSTransitionReplace from "react-css-transition-replace";
@@ -21,17 +21,17 @@ class App extends Component {
       results: [[]],
       current_results_page: 0,
       fields: [[]],
-      eventID: 128520,
+      eventID: 130557,
       eventName: null
     };
   }
   componentDidMount() {
     let that = this;
-    Repeat(function (done) {
+    Repeat(function(done) {
       axios
         .get(
           "https://radiant-forest-25626.herokuapp.com/results/" +
-          that.state.eventID
+            that.state.eventID
         )
         .then(e => {
           let fields = [];
@@ -70,7 +70,7 @@ class App extends Component {
     })
       .every(5, "sec")
       .start.now();
-    Repeat(function () {
+    Repeat(function() {
       if (that.state.results.length - 1 > that.state.current_results_page) {
         that.setState({
           current_results_page: that.state.current_results_page + 1
@@ -116,7 +116,7 @@ class App extends Component {
                                     key={`cell-${index}`}
                                     className={
                                       this.state.fields[index] ===
-                                        "Age Group Place"
+                                      "Age Group Place"
                                         ? `agPlace${i}`
                                         : null
                                     }
@@ -130,10 +130,10 @@ class App extends Component {
                         }
                       )
                     ) : (
-                        <tr>
-                          <td>Results Not Available</td>
-                        </tr>
-                      )}
+                      <tr>
+                        <td>Results Not Available</td>
+                      </tr>
+                    )}
                   </tbody>
                 </table>
               </ReactCSSTransitionReplace>
@@ -144,7 +144,7 @@ class App extends Component {
             className="column has-text-centered is-one-quarter"
           >
             <img
-              src="https://2tr8so2439vc34xre1h2sdi1-wpengine.netdna-ssl.com/wp-content/uploads/2018/09/she-power-fierce-strong-brave.png"
+              src={summerLogo}
               style={{ marginTop: "20px", width: "300px" }}
               alt=""
               key="eventImage"
@@ -154,7 +154,7 @@ class App extends Component {
               style={{ marginTop: "20px" }}
               key="eventName"
             >
-              SHE Power
+              Summer Night Run
             </h1>
             <h2 className="subtitle is-5" key="eventResults">
               Results
@@ -180,8 +180,8 @@ class App extends Component {
         </div>
       </div>
     ) : (
-        <img src={Logo} alt="background" />
-      );
+      <img src={Logo} alt="background" />
+    );
   }
 }
 
