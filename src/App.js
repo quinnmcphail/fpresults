49a17597819696fd7dfdc88ba4e426050e828334
Quinn McPhail <quinnmcphail@me.com>
2021-01-09 18:14:19 -0500

    Winter 2021

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
-import * as shot from "./shot.png";
+import * as winter from "./winterNight.png";
 import * as Logo from "./resultsBack.svg";
 import Repeat from "repeat";
 import ReactCSSTransitionReplace from "react-css-transition-replace";
@@ -21,7 +21,7 @@ class App extends Component {
       results: [[]],
       current_results_page: 0,
       fields: [[]],
-      eventID: 163179,
+      eventID: 164179,
       eventName: null,
     };
   }
@@ -147,7 +147,7 @@ class App extends Component {
             className="column has-text-centered is-one-quarter"
           >
             <img
-              src={shot}
+              src={winter}
               style={{ marginTop: "20px", width: "300px" }}
               alt=""
               key="eventImage"
@@ -157,7 +157,7 @@ class App extends Component {
               style={{ marginTop: "20px" }}
               key="eventName"
             >
-              Shot in the Dark Whiskey Run
+              Winter Night Trail Run
             </h1>
             <h2 className="subtitle is-5" key="eventResults">
               Results
