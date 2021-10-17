c9d44cce195953c2700d70f61a361d9213b11ff8
Quinn McPhail <quinnmcphail@me.com>
2021-10-17 10:30:17 -0400

    Brew

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
       results: [[]],
       current_results_page: 0,
       fields: [[]],
-      eventID: 183566,
+      eventID: 184223,
       eventName: null,
     };
   }
@@ -147,7 +147,7 @@ class App extends Component {
             className="column has-text-centered is-one-quarter"
           >
             <img
-              src="https://www.131events.com/wp-content/uploads/2021/04/dnf-logo.jpg"
+              src="https://www.131events.com/wp-content/uploads/2019/06/brew-halloween-logo-2019.jpg"
               style={{ marginTop: "20px", width: "300px" }}
               alt=""
               key="eventImage"
@@ -157,7 +157,7 @@ class App extends Component {
               style={{ marginTop: "20px" }}
               key="eventName"
             >
-              DNF Trail Run
+              Brew HaHa Halloween 5K
             </h1>
             <h2 className="subtitle is-5" key="eventResults">
               Results
