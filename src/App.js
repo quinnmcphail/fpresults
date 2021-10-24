b5de2185ee7517e95dbcf4b2300b1f89a04b3c66
Quinn McPhail <quinnmcphail@me.com>
2021-10-24 09:11:14 -0400

    AZ48

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
       results: [[]],
       current_results_page: 0,
       fields: [[]],
-      eventID: 184223,
+      eventID: 184823,
       eventName: null,
     };
   }
@@ -147,7 +147,7 @@ class App extends Component {
             className="column has-text-centered is-one-quarter"
           >
             <img
-              src="https://www.131events.com/wp-content/uploads/2019/06/brew-halloween-logo-2019.jpg"
+              src="https://www.az48marathon.com/wp-content/uploads/sites/3/2019/10/az48-logo-white.jpg"
               style={{ marginTop: "20px", width: "300px" }}
               alt=""
               key="eventImage"
@@ -157,7 +157,7 @@ class App extends Component {
               style={{ marginTop: "20px" }}
               key="eventName"
             >
-              Brew HaHa Halloween 5K
+              Chandler #AZ48 Half Marathon & 5K
             </h1>
             <h2 className="subtitle is-5" key="eventResults">
               Results
