64bfbf227747158dbc5205f1633c200ea56e59ba
Quinn McPhail <14959853+quinnmcphail@users.noreply.github.com>
2022-05-14 09:48:30 -0400

    froyo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import * as Logo from "./resultsBack.svg";
-// import * as hakuna from "./hakuna.png";
+import * as froyo from "./froyo.svg";
 import Repeat from "repeat";
 import ReactCSSTransitionReplace from "react-css-transition-replace";
 
@@ -21,7 +21,7 @@ class App extends Component {
       results: [[]],
       current_results_page: 0,
       fields: [[]],
-      eventID: 200917,
+      eventID: 202053,
       eventName: null,
     };
   }
@@ -147,7 +147,7 @@ class App extends Component {
             className="column has-text-centered is-one-quarter"
           >
             <img
-              src="https://www.131events.com/wp-content/uploads/2021/02/cinco-logo-2021-no-words.jpg"
+              src={ froyo }
               style={{ marginTop: "20px", width: "300px" }}
               alt=""
               key="eventImage"
@@ -157,7 +157,7 @@ class App extends Component {
               style={{ marginTop: "20px" }}
               key="eventName"
             >
-              Cinco De Miles 5K
+              ZOYO FroYo 5K
             </h1>
             <h2 className="subtitle is-5" key="eventResults">
               Results
