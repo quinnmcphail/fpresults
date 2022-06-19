08dac50625d386cfe81a24aab4cf4b5dd58d0528
Quinn McPhail <14959853+quinnmcphail@users.noreply.github.com>
2022-06-18 23:45:52 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import * as Logo from "./resultsBack.svg";
-import * as honor from "./honor.png";
+import * as shePower from "./shePower.png";
 import Repeat from "repeat";
 import ReactCSSTransitionReplace from "react-css-transition-replace";
 
@@ -21,7 +21,7 @@ class App extends Component {
       results: [[]],
       current_results_page: 0,
       fields: [[]],
-      eventID: 203954,
+      eventID: 208797,
       eventName: null,
     };
   }
@@ -147,7 +147,7 @@ class App extends Component {
             className="column has-text-centered is-one-quarter"
           >
             <img
-              src={ honor }
+              src={ shePower }
               style={{ marginTop: "20px", width: "300px" }}
               alt=""
               key="eventImage"
@@ -157,7 +157,7 @@ class App extends Component {
               style={{ marginTop: "20px" }}
               key="eventName"
             >
-              Honor the Brave 5K
+              SHE Power Half &amp; 5K
             </h1>
             <h2 className="subtitle is-5" key="eventResults">
               Results
