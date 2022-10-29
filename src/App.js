d716b54a49c1ff5f48ab33eb4ceb8d9da7f63dba
Quinn McPhail <14959853+quinnmcphail@users.noreply.github.com>
2022-10-29 04:57:21 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
       results: [[]],
       current_results_page: 0,
       fields: [[]],
-      eventID: 224116,
+      eventID: 224662,
       eventName: null,
     };
   }
@@ -147,7 +147,7 @@ class App extends Component {
             className="column has-text-centered is-one-quarter"
           >
             <img
-              src="https://www.az48marathon.com/wp-content/uploads/sites/3/2019/10/az48-logo-white.jpg"
+              src="https://www.131events.com/wp-content/uploads/2022/07/brew-2022-logo.jpeg"
               style={{ marginTop: "20px", width: "300px" }}
               alt=""
               key="eventImage"
@@ -157,7 +157,7 @@ class App extends Component {
               style={{ marginTop: "20px" }}
               key="eventName"
             >
-              #AZ48 Half & 5K
+              Brew HaHa 5K
             </h1>
             <h2 className="subtitle is-5" key="eventResults">
               Results
