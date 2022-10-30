13c0f068316b5b951b67b6876e4bcf8cabaca270
Quinn McPhail <14959853+quinnmcphail@users.noreply.github.com>
2022-10-30 09:30:09 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
       results: [[]],
       current_results_page: 0,
       fields: [[]],
-      eventID: 224662,
+      eventID: 224752,
       eventName: null,
     };
   }
