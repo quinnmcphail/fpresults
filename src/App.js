4d815b2455aa2f62eb69777da9721078ee251ff1
Quinn McPhail <14959853+quinnmcphail@users.noreply.github.com>
2024-05-05 15:11:55 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import * as Logo from "./resultsBack.svg";
-import * as SHE from "./hakuna.png";
+// import * as SHE from "./hakuna.png";
 import Repeat from "repeat";
 import ReactCSSTransitionReplace from "react-css-transition-replace";
 
@@ -21,7 +21,7 @@ class App extends Component {
       results: [[]],
       current_results_page: 0,
       fields: [[]],
-      eventID: 284715,
+      eventID: 289301,
       eventName: null,
     };
   }
@@ -147,7 +147,7 @@ class App extends Component {
             className="column has-text-centered is-one-quarter"
           >
             <img
-              src={SHE}
+              src="https://www.131events.com/wp-content/uploads/2024/03/cinco-icon-2024.jpg"
               style={{ marginTop: "20px", width: "300px" }}
               alt=""
               key="eventImage"
@@ -157,7 +157,7 @@ class App extends Component {
               style={{ marginTop: "20px" }}
               key="eventName"
             >
-              Hakuna Mimosa 5K
+              Cinco de Miles 5K
             </h1>
             <h2 className="subtitle is-5" key="eventResults">
               Results
