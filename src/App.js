7f3ff5042d5a9fc749ccbc7bfc47aa0dd3384fca
Quinn McPhail <14959853+quinnmcphail@users.noreply.github.com>
2024-11-24 09:52:53 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import * as Logo from "./resultsBack.svg";
-// import * as SHE from "./shePower.png";
+import * as SHE from "./donut.png";
 import Repeat from "repeat";
 import ReactCSSTransitionReplace from "react-css-transition-replace";
 
@@ -21,7 +21,7 @@ class App extends Component {
       results: [[]],
       current_results_page: 0,
       fields: [[]],
-      eventID: 316900,
+      eventID: 318652,
       eventName: null,
     };
   }
@@ -147,7 +147,7 @@ class App extends Component {
             className="column has-text-centered is-one-quarter"
           >
             <img
-              src="https://www.131events.com/wp-content/uploads/2024/07/shot-logo-2024.jpg"
+              src={SHE}
               style={{ marginTop: "20px", width: "300px" }}
               alt=""
               key="eventImage"
@@ -157,7 +157,7 @@ class App extends Component {
               style={{ marginTop: "20px" }}
               key="eventName"
             >
-              Shot in the Dark 5K
+              Donut Judge Me 5K
             </h1>
             <h2 className="subtitle is-5" key="eventResults">
               Results
