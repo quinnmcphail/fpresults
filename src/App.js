34f921197a63f0a80d2a75e3604bed98f6eb4236
Quinn McPhail <14959853+quinnmcphail@users.noreply.github.com>
2025-04-27 10:53:07 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
       results: [[]],
       current_results_page: 0,
       fields: [[]],
-      eventID: 333723,
+      eventID: 337634,
       eventName: null,
     };
   }
@@ -148,7 +148,7 @@ class App extends Component {
             className="column has-text-centered is-one-quarter"
           >
             <img
-              src="https://hangdogrunning.com/wp-content/uploads/rum-rum-2025-icon.jpg"
+              src="https://hangdogrunning.com/wp-content/uploads/hakuna-2025-icon.jpg"
               style={{ marginTop: "20px", width: "300px" }}
               alt=""
               key="eventImage"
@@ -158,7 +158,7 @@ class App extends Component {
               style={{ marginTop: "20px" }}
               key="eventName"
             >
-              Rum Run 5K
+              Hakuna Mimosa 5K
             </h1>
             <h2 className="subtitle is-5" key="eventResults">
               Results
