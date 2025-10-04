176bbdebc4fec4fb44bae0268ba82fb85c080458
Quinn McPhail <14959853+quinnmcphail@users.noreply.github.com>
2025-10-04 10:44:41 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
       results: [[]],
       current_results_page: 0,
       fields: [[]],
-      eventID: 350796,
+      eventID: 365144,
       eventName: null,
     };
   }
@@ -148,7 +148,7 @@ class App extends Component {
             className="column has-text-centered is-one-quarter"
           >
             <img
-              src="https://hangdogrunning.com/wp-content/uploads/summer-logo-2025.jpg"
+              src="https://hangdogrunning.com/wp-content/uploads/djm-gilbert-icon.jpg"
               style={{ marginTop: "20px", width: "300px" }}
               alt=""
               key="eventImage"
@@ -158,7 +158,7 @@ class App extends Component {
               style={{ marginTop: "20px" }}
               key="eventName"
             >
-              Summer Night Trail Run
+              Donut Judge Me 5K
             </h1>
             <h2 className="subtitle is-5" key="eventResults">
               Results
