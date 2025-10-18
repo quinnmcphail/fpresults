bc9c192c175f4ad9ea04359117a9e3f5faba6ea1
Quinn McPhail <14959853+quinnmcphail@users.noreply.github.com>
2025-10-18 09:15:48 -0700

    Brew

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
       results: [[]],
       current_results_page: 0,
       fields: [[]],
-      eventID: 365145,
+      eventID: 368007,
       eventName: null,
     };
   }
@@ -148,7 +148,7 @@ class App extends Component {
             className="column has-text-centered is-one-quarter"
           >
             <img
-              src="https://hangdogrunning.com/wp-content/uploads/djm-orovalley-icon.jpg"
+              src="https://hangdogrunning.com/wp-content/uploads/2025-brew-icon.jpg"
               style={{ marginTop: "20px", width: "300px" }}
               alt=""
               key="eventImage"
@@ -158,7 +158,7 @@ class App extends Component {
               style={{ marginTop: "20px" }}
               key="eventName"
             >
-              Donut Judge Me 5K
+              Brew HaHa 5K
             </h1>
             <h2 className="subtitle is-5" key="eventResults">
               Results
